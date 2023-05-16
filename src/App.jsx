4c4099b160afd2a59ccade9571dd673026477ba9
torcoroma <torcoromaarias@gmail.com>
2023-05-16 12:19:33 -0500

    Ejercicio 13

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Body } from "./ejercicio1/Body";
 import { Ejercicio10 } from "./ejercicio10/Ejercicio10";
 import { Ejercicio11 } from "./ejercicio11/Ejercicio11";
 import { Card } from "./ejercicio12/Card";
+import { Ejercicio13 } from "./ejercicio13/Ejercicio13";
 import { Ejerct2 } from "./ejercicio2/Ejerct2";
 import { Ejercici3 } from "./ejercicio3/Ejercici3";
 import { Ejercicio4 } from "./ejercicio4/Ejercicio4";
@@ -85,6 +86,16 @@ function App() {
           mostrarse su información detallada en una sección aparte.
         </p>
         <Card />
+      </div>
+      <div className="container border pt-4">
+        <h2>Ejercicio 13</h2>
+        <p>
+          Crear un componente que reciba como propiedad un arreglo de objetos
+          con información de usuarios y muestre una lista con sus nombres y
+          correos electrónicos. Al hacer clic en un usuario de la lista, debe
+          mostrarse su información detallada en una sección aparte.
+        </p>
+        <Ejercicio13 />
       </div>
     </>
   );
