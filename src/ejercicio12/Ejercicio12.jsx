f112897747e5ea4261f8dafc37799e3b53159d46
torcoroma <torcoromaarias@gmail.com>
2023-05-22 17:58:31 -0500

    carrito lsto

--- src/ejercicio12/Ejercicio12.jsx
+++ src/ejercicio12/Ejercicio12.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
-import { Cart } from "./Cart";
 import { Product } from "./Product";
 import products from "./products";
-import { BsFillCartFill } from "react-icons/bs";
-// import { BsFillCartFill } from "react-icons/fa";
 import "./Cart.css";
+import { Cart } from "./Cart";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 export const Ejercicio12 = () => {
   const [cartItems, setCartItems] = useState([]);
 
@@ -17,16 +18,29 @@ export const Ejercicio12 = () => {
     updatedCartItems.splice(index, 1);
     setCartItems(updatedCartItems);
   };
+
   return (
     <div>
-      <Cart cartItems={cartItems} removeFromCart={removeFromCart} />
-      <h2 className="text-center text-info">Carrito de compras</h2>
-      <BsFillCartFill />
-      <div className=" cart">
-        {products.map((product) => (
-          <Product key={product.id} product={product} addToCart={addToCart} />
-        ))}
-      </div>
+      <Container>
+        <h2 className="text-center text-info">Carrito de compras</h2>
+        <Row>
+          <Col lg={8}>
+            {" "}
+            <div className=" cart">
+              {products.map((product) => (
+                <Product
+                  key={product.id}
+                  product={product}
+                  addToCart={addToCart}
+                />
+              ))}
+            </div>
+          </Col>
+          <Col>
+            <Cart cartItems={cartItems} removeFromCart={removeFromCart} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
